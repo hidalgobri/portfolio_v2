199f1b1b6ae1b032210b9ff3b63fe2ce85d1e7b2
Valeria Hidalgo <valeriahidalgo50@gmail.com>
2022-05-13 13:01:20 -0500

    hamburguer menu

---
+++ script.js
@@ -0,0 +1,39 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+let aplastoHamburguer = false;
+var prevScrollpos = window.pageYOffset;
+
+// Para el menu hamburguesa
+hamburguer.addEventListener("click", () => {
+  hamburguer.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  aplastoHamburguer= !aplastoHamburguer;
+  console.log(aplastoHamburguer)
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+
+  n.addEventListener("click", () => {
+    hamburguer.classList.remove("active");
+    console.log("este")
+    navMenu.classList.remove("active");
+  })
+);
+
+// Para que no se muestre el navbar cuando baja
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    console.log("sube")
+    document.getElementById("navbar").style.top = "0";
+    
+  }
+  else if(aplastoHamburguer && (prevScrollpos < currentScrollPos )){ //esta bajando
+      document.getElementById("navbar").style.top = "0";
+      console.log("baja y aplasto")
+  } 
+  else {
+    document.getElementById("navbar").style.top = "-80px";
+    console.log("baja")
+  }
+  prevScrollpos = currentScrollPos;
+};
